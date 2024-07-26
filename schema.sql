d0659ceb3722239328aec88c17cbaa506c91dac5
katjamva <katja.myrskyvaara@gmail.com>
2024-07-26 12:13:52 +0300

    Create tables users and restaurant in schema.sql, finish initial index.html, finish initial layout.html, create and begin register.html

--- schema.sql
+++ schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE users (
     username TEXT, 
     password TEXT
     );
+
+CREATE TABLE restaurants (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    location TEXT,
+    opening_hours TEXT,
+    created_at TIMESTAMP
+);
+
